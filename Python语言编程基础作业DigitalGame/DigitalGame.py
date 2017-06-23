510d97d32e2dfd493a4bf612b777702abeaae191
bo <18910338836@163.com>
2017-06-23 08:37:07 +0800

    猜字游戏修改

--- Python语言编程基础作业DigitalGame/DigitalGame.py
+++ Python语言编程基础作业DigitalGame/DigitalGame.py
@@ -1,32 +1,3 @@
-import random
-
-# inputNum = input('请随意输入一个整数整数')
-# intInputNum = int(inputNum)
-#
-# minNum = 0
-# maxNum = 100
-#
-#
-#
-# def playGame():
-#
-#
-#
-#
-#  print(randomNum)
-#  result(intInputNum,minNum,maxNum)
-#
-#
-#
-# def result(intInputNum,minNum,maxNum):
-#     if intInputNum > minNum and intInputNum < maxNum:
-#         print('你猜的数字在范围内')
-#     else:
-#         print('你猜的范围不对')
-#         # playGame()
-#
-#
-# playGame()
 
 
 # 1.在设定的范围内生成随机数
